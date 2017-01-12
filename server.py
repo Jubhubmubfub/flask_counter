552466776dbfebd7ed764ec5314f04f811867911
Marcus Sakoda <Jubhub@yahool.com>
2017-01-11 16:37:48 -0800

    added +2 button

--- server.py
+++ server.py
@@ -1,9 +1,20 @@
 from flask import Flask, render_template, request, redirect, session
 app = Flask(__name__)
 app.secret_key = "ThisIsSecret"
-
+#
 @app.route('/')
 def index():
+    if session['count']:
+        session['count'] += 1
+    else:
+        session['count'] = 1
     return render_template('index.html')
-    
+
+@app.route('/plusone')
+def plusone():
+    session['count'] +=1
+
+    return redirect('/')
+
+
 app.run(debug=True)
